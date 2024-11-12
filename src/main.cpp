65a8de77e1a411f8f18dbfef19f72e74385a10ea
Eugene Hong <awidesky@naver.com>
2024-11-12 20:21:04 +0900

    remove debug print & edit cmake

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char *argv[]) {
     std::cout << "Usage : " << argv[0] << " <filenames>" << std::endl;
     return 0;
   }
-  std::cout << "fileSize : " << getSize(argv[1]) << std::endl;
 
   for (int i = 1; i < argc; i++) {
     std::cout << argv[i] << " :" << std::endl;
