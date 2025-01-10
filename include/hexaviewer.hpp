ad62a9daa4233d8d847e3294e50845764133606c
Eugene Hong <awidesky@naver.com>
2025-01-10 17:23:22 +0900

    test stub

---
+++ include/hexaviewer.hpp
@@ -0,0 +1,109 @@
+#ifndef HEXAVIEWER_HPP
+#define HEXAVIEWER_HPP
+
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+
+#include <filesystem>
+
+namespace hexa {
+/* https://stackoverflow.com/questions/5840148/how-can-i-get-a-files-size-in-c */
+static inline int getSize(const char* path) {
+  // std::ifstream in(path, std::ifstream::ate | std::ifstream::binary);
+  // return in.tellg();
+  return std::filesystem::file_size({path});
+}
+
+class hexaViewer {
+
+private:
+  std::ifstream fs;
+  /** Length of the buffer */
+  int length;
+  int cnt = 0;
+  int rowNumLength;
+  unsigned char* buf;
+  std::stringstream strStream;
+
+public:
+  static constexpr int DEFAULTBUFFERSIZE = 64 * 1024;
+
+  /** Set buffer size to the length to DEFAULTBUFFERSIZE */
+  hexaViewer(std::string path) : hexaViewer(path.c_str(), DEFAULTBUFFERSIZE) {}
+  /** Set buffer size to the length to DEFAULTBUFFERSIZE */
+  hexaViewer(const char* path) : hexaViewer(path, DEFAULTBUFFERSIZE) {}
+  /** Set buffer size to the given parameter */
+  hexaViewer(const char* path, int bufferSize) : fs(path, std::ifstream::binary), length(bufferSize), rowNumLength((int)(std::log(getSize(path)) / std::log(16)) + 1) {
+    if (fs) {
+      buf = new unsigned char[length];
+
+      for (int i = 0; i < rowNumLength + 3; i++)
+        strStream << ' ';
+
+      strStream << "01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10\n";
+
+      for (int i = 0; i < 50 + rowNumLength; i++)
+        strStream << '-';
+        
+      strStream << std::hex << std::setfill('0') << std::uppercase;
+    }
+  }
+
+  ~hexaViewer() {
+    delete buf;
+  }
+
+  operator bool() const {
+    return (bool)fs;
+  }
+
+  /** Read all content of the file */
+  hexaViewer& readAll() {
+    while (readOnce() > 0) {
+    }
+
+    return *this;
+  }
+  /** Read the file once with the buffer(try filling it).
+   * return number of characters. Or 0 if EOF, else -1 on error.
+   */
+  int readOnce() {
+    fs.read((char*)buf, length);
+    std::streamsize dataSize = fs.gcount();
+    if (dataSize != 0) {
+      // std::cout << "read : " << dataSize << " bytes\n";
+      for (int i = 0; i < dataSize; i++, cnt++) {
+        if (!(cnt % 16))
+          strStream << '\n'
+                    << std::setw(rowNumLength)
+                    << cnt << " | ";
+
+        strStream << std::setw(2) << (int)buf[i] << ' ';
+      }
+      return dataSize;
+    } else if (fs.eof()) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  /** Return the string . */
+  std::string copyResult() const {
+    return strStream.str();
+  }
+  /** Return the string, and deletes it.
+   * Use this when you don't need any copy of the string in this object.
+   * Metadata(e.g. position of file) will not be deleted
+   */
+  std::string pollResult() {
+    std::string str = strStream.str();
+    strStream.str("");
+    strStream.clear();
+    return str;
+  }
+};
+
+} // namespace hexa
+#endif
--- include/hexaviewer_fast.hpp
+++ include/hexaviewer_fast.hpp
@@ -1,53 +1,58 @@
+#ifndef HEXAVIEWER_HPP
+#define HEXAVIEWER_HPP
+
 #include <fstream>
-#include <sstream>
 #include <iomanip>
+#include <sstream>
 
 #include <filesystem>
 
+namespace hexa_fast {
 /* https://stackoverflow.com/questions/5840148/how-can-i-get-a-files-size-in-c */
-static inline int getSize(const char *path) {
+static inline int getSize(const char* path) {
   // std::ifstream in(path, std::ifstream::ate | std::ifstream::binary);
   // return in.tellg();
   return std::filesystem::file_size({path});
 }
 
-class FileReader { //TODO : Hexaviewer로 바꾸기
+class hexaViewer {
 
 private:
   std::ifstream fs;
   /** Length of the buffer */
   int length;
   int cnt = 0;
-  int rowNumLength;
-  unsigned char *buf;
+  const int rowNumLength;
+  unsigned char* buf;
   std::stringstream strStream;
 
+  //static constexpr char* ROWHEXTABLE[] = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B", "0C", "0D", "0E", "0F", "10"};
+  
 public:
   static constexpr int DEFAULTBUFFERSIZE = 64 * 1024;
 
   /** Set buffer size to the length to DEFAULTBUFFERSIZE */
-  FileReader(std::string path) : FileReader(path.c_str(), DEFAULTBUFFERSIZE) {}
+  hexaViewer(std::string path) : hexaViewer(path.c_str(), DEFAULTBUFFERSIZE) {}
   /** Set buffer size to the length to DEFAULTBUFFERSIZE */
-  FileReader(const char *path) : FileReader(path, DEFAULTBUFFERSIZE) {}
+  hexaViewer(const char* path) : hexaViewer(path, DEFAULTBUFFERSIZE) {}
   /** Set buffer size to the given parameter */
-  FileReader(const char *path, int bufferSize) : fs(path, std::ifstream::binary), length(bufferSize), rowNumLength((int)(std::log(getSize(path)) / std::log(16)) + 1) {
+  hexaViewer(const char* path, int bufferSize) : fs(path, std::ifstream::binary), length(bufferSize), rowNumLength((int)(std::log(getSize(path)) / std::log(16)) + 1) {
     if (fs) {
       buf = new unsigned char[length];
 
-      strStream << std::hex << std::setfill('0') << std::uppercase;
       for (int i = 0; i < rowNumLength + 3; i++)
-        strStream << " ";
+        strStream << ' ';
 
-      for (int i = 1; i < 17; i++) { // add column index
-        strStream << std::setw(2) << i << " ";
-      }
-      strStream << "\n";
+      strStream << "01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10\n";
+
       for (int i = 0; i < 50 + rowNumLength; i++)
-        strStream << "-";
+        strStream << '-';
+        
+      strStream << std::hex << std::setfill('0') << std::uppercase;
     }
   }
 
-  ~FileReader() {
+  ~hexaViewer() {
     delete buf;
   }
 
@@ -56,7 +61,7 @@ public:
   }
 
   /** Read all content of the file */
-  FileReader &readAll() {
+  hexaViewer& readAll() {
     while (readOnce() > 0) {
     }
 
@@ -72,11 +77,11 @@ public:
       // std::cout << "read : " << dataSize << " bytes\n";
       for (int i = 0; i < dataSize; i++, cnt++) {
         if (!(cnt % 16))
-          strStream << "\n"
+          strStream << '\n'
                     << std::setw(rowNumLength)
                     << cnt << " | ";
 
-        strStream << std::setw(2) << (int)buf[i] << " ";
+        strStream << std::setw(2) << (int)buf[i] << ' ';
       }
       return dataSize;
     } else if (fs.eof()) {
@@ -101,3 +106,6 @@ public:
     return str;
   }
 };
+
+} // namespace hexa
+#endif
